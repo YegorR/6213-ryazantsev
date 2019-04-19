bfc70b49c865a2942d5bf2d5fd60ecec1d067c8b
Yegor Ryazancev <egorryazancev1908@mail.ru>
2019-04-19 11:40:28 +0700

    FigureImpl - реализованы getDescription и addToDescription

--- task2/src/main/java/ru/cft/focusstart/ryazantsev/figure/FigureImpl.java
+++ task2/src/main/java/ru/cft/focusstart/ryazantsev/figure/FigureImpl.java
@@ -1,5 +1,25 @@
 package ru.cft.focusstart.ryazantsev.figure;
 
-abstract class FigureImpl {
+import java.util.HashMap;
+import java.util.Map;
 
+public abstract class FigureImpl implements Figure {
+
+    private Map<String, Object> descriptionLines = new HashMap<>();
+
+    @Override
+    public final String getDescription() {
+        StringBuilder description = new StringBuilder();
+        for(Map.Entry<String, Object> entry: descriptionLines.entrySet()){
+            description.append(entry.getKey());
+            description.append(": ");
+            description.append(entry.getValue());
+            description.append("\n");
+        }
+        return description.toString();
+    }
+
+    protected final void addToDescription(String valueDescription, Object value){
+        descriptionLines.put(valueDescription, value);
+    }
 }
