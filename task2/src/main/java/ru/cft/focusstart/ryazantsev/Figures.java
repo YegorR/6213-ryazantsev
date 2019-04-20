22e1b628358c31eff4aa1d4b0910dae09a36894e
Yegor Ryazancev <egorryazancev1908@mail.ru>
2019-04-20 23:44:27 +0700

    Реализована логика внутри main метода

--- task2/src/main/java/ru/cft/focusstart/ryazantsev/Figures.java
+++ task2/src/main/java/ru/cft/focusstart/ryazantsev/Figures.java
@@ -1,7 +1,43 @@
 package ru.cft.focusstart.ryazantsev;
 
+import ru.cft.focusstart.ryazantsev.exception.*;
+import ru.cft.focusstart.ryazantsev.figure.Figure;
+
+
 public class Figures {
     public static void main(String[] args) {
+        try {
+            if (args.length < 1) {
+                throw new NoArgsException();
+            }
 
+            ArgsReader argsReader = new ArgsReader(args[0]);
+            argsReader.read();
+            Figure figure = FigureCreator.createFigure(argsReader.getFigureName(), argsReader.getParameters());
+
+            if (args.length < 2) {
+                System.out.print(figure.getDescription());
+            } else {
+                DescriptionWriter.writeDescription(args[1], figure.getDescription());
+            }
+        } catch (NoArgsException ex) {
+            System.err.println("The input file name is not specified!");
+        } catch (InputFileNotFoundException ex) {
+            System.err.println("The input file is not found!");
+        } catch (EmptyInputFileException ex) {
+            System.err.println("The input file is empty!");
+        } catch (IOInputException ex) {
+            System.err.println("The IOException of the input file!");
+        } catch (IOOutputException ex) {
+            System.err.println("The IOException of the output file!");
+        } catch (NotEnoughParametersException ex) {
+            System.err.println("Not enough parameters in the input file for this figure!");
+        } catch (OutputFileNotFoundException ex) {
+            System.err.println("The output file is not found!");
+        } catch (WrongFigureNameException ex) {
+            System.err.print("The specified figure name is wrong!");
+        } catch (WrongParametersException ex) {
+            System.err.println("The specified parameters are wrong!");
+        }
     }
 }
