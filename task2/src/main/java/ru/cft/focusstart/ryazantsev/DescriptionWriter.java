11044cfaca3f84af83f5e7af875a6ea733fa83b5
Yegor Ryazancev <egorryazancev1908@mail.ru>
2019-04-27 09:47:58 +0700

    Исключения в DescriptionWriter исправлены на FileException

--- task2/src/main/java/ru/cft/focusstart/ryazantsev/DescriptionWriter.java
+++ task2/src/main/java/ru/cft/focusstart/ryazantsev/DescriptionWriter.java
@@ -1,20 +1,20 @@
 package ru.cft.focusstart.ryazantsev;
 
-import ru.cft.focusstart.ryazantsev.exception.IOOutputException;
-import ru.cft.focusstart.ryazantsev.exception.OutputFileNotFoundException;
+
+import ru.cft.focusstart.ryazantsev.exception.FileException;
 
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class DescriptionWriter {
-    public static void writeDescription(String filename, String description) throws OutputFileNotFoundException, IOOutputException {
+    public static void writeDescription(String filename, String description) throws FileException {
         try (FileWriter fw = new FileWriter(filename)) {
             fw.write(description);
         } catch (FileNotFoundException ex) {
-            throw new OutputFileNotFoundException();
+            throw new FileException("The output file is not found!");
         } catch (IOException ex) {
-            throw new IOOutputException();
+            throw new FileException("The IOException of the output file!");
         }
     }
 }
