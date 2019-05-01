d0df7861968e26571659e264f2e65aece1ecad86
Yegor Ryazancev <egorryazancev1908@mail.ru>
2019-05-01 15:13:24 +0700

    В Triangle для вычисления угла используется теперь calculateAngle()

--- task2/src/main/java/ru/cft/focusstart/ryazantsev/figure/Triangle.java
+++ task2/src/main/java/ru/cft/focusstart/ryazantsev/figure/Triangle.java
@@ -27,11 +27,11 @@ public class Triangle extends FigureImpl {
         addToDescription("Площадь", calculateSquare());
         addToDescription("Периметр", calculatePerimeter());
         addToDescription("Сторона a", a);
-        addToDescription("Угол напротив стороны a", calculateAngleA());
+        addToDescription("Угол напротив стороны a", calculateAngle(a, b, c));
         addToDescription("Сторона b", b);
-        addToDescription("Угол напротив стороны b", calculateAngleB());
+        addToDescription("Угол напротив стороны b", calculateAngle(b, a, c));
         addToDescription("Сторона c", c);
-        addToDescription("Угол напротив стороны c", calculateAngleC());
+        addToDescription("Угол напротив стороны c", calculateAngle(c, a, b));
     }
 
     private double calculateSquare() {
@@ -43,19 +43,9 @@ public class Triangle extends FigureImpl {
         return a + b + c;
     }
 
-    private double calculateAngleA() {
-        double angle = Math.acos((b * b + c * c - a * a) / (2 * b * c));
-        return Math.toDegrees(angle);
-    }
-
-    private double calculateAngleB() {
-        double angle = Math.acos((a * a + c * c - b * b) / (2 * a * c));
-        return Math.toDegrees(angle);
-    }
-
-    private double calculateAngleC() {
-        double angle = Math.acos((a * a + b * b - c * c) / (2 * a * b));
-        return Math.toDegrees(angle);
+    private static double calculateAngle(double oppositeSide, double leftSide, double rightSide) {
+        return Math.toDegrees(Math.acos((leftSide * leftSide + rightSide * rightSide - oppositeSide * oppositeSide) /
+                (2 * leftSide * rightSide)));
     }
 
     @Override
