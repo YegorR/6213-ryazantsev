b50f7bc1b333d96cbef8488340498124b5468bb7
Yegor Ryazancev <egorryazancev1908@mail.ru>
2019-05-09 13:55:28 +0700

    Создан класс FieldView

---
+++ task3/src/main/java/ru/cft/focusstart/ryazantsev/view/FieldView.java
@@ -0,0 +1,14 @@
+package ru.cft.focusstart.ryazantsev.view;
+
+
+import ru.cft.focusstart.ryazantsev.logic.GameStatus;
+import ru.cft.focusstart.ryazantsev.logic.IntCouple;
+import ru.cft.focusstart.ryazantsev.logic.ViewCellValues;
+
+import java.util.Map;
+
+public class FieldView {
+    public void updateCells(Map<IntCouple, ViewCellValues> cells, GameStatus status) {
+
+    }
+}
