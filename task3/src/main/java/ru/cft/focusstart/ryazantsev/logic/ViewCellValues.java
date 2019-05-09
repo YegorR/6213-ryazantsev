1023f866e57309899d2ff2891562b190b07f214e
Yegor Ryazancev <egorryazancev1908@mail.ru>
2019-05-09 16:31:35 +0700

    Добавлен метод getValueOfNumber для ViewCellValues

--- task3/src/main/java/ru/cft/focusstart/ryazantsev/logic/ViewCellValues.java
+++ task3/src/main/java/ru/cft/focusstart/ryazantsev/logic/ViewCellValues.java
@@ -1,5 +1,31 @@
 package ru.cft.focusstart.ryazantsev.logic;
 
+
 public enum ViewCellValues {
-    UNTOUCHED, ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, FLAG, MINE, NOMINE
+    UNTOUCHED, ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, FLAG, MINE, NOMINE;
+
+    public static ViewCellValues getValueOfNumber(int numberValue) {
+        switch (numberValue) {
+            case CellValues.ZERO:
+                return ZERO;
+            case CellValues.ONE:
+                return ONE;
+            case CellValues.TWO:
+                return TWO;
+            case CellValues.THREE:
+                return THREE;
+            case CellValues.FOUR:
+                return FOUR;
+            case CellValues.FIVE:
+                return FIVE;
+            case CellValues.SIX:
+                return SIX;
+            case CellValues.SEVEN:
+                return SEVEN;
+            case CellValues.EIGHT:
+                return EIGHT;
+            default:
+                return null;
+        }
+    }
 }
