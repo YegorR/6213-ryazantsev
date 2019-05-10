1420a49103deeb701759e82ec8f1143d8bf7e215
Yegor Ryazancev <egorryazancev1908@mail.ru>
2019-05-10 18:45:11 +0700

    Реализован класс FieldLogic

--- task3/src/main/java/ru/cft/focusstart/ryazantsev/logic/FieldLogic.java
+++ task3/src/main/java/ru/cft/focusstart/ryazantsev/logic/FieldLogic.java
@@ -119,12 +119,173 @@ package ru.cft.focusstart.ryazantsev.logic;
 
 import ru.cft.focusstart.ryazantsev.view.FieldView;
 
-public class FieldLogic {
-    public FieldLogic(int[][] field, FieldView FieldView) {
+import static ru.cft.focusstart.ryazantsev.logic.CellValues.*;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class FieldLogic {
+    private int[][] field;
+    private StatusCellValue[][] statusField;
+    private FieldView fieldView;
+
+    public FieldLogic(int[][] field, FieldView fieldView) {
+        this.field = field;
+        this.fieldView = fieldView;
+
+        statusField = new StatusCellValue[field.length][field[0].length];
+        for (StatusCellValue[] row : statusField) {
+            Arrays.fill(row, StatusCellValue.UNTOUCHED);
+        }
     }
 
     public void pressCell(IntCouple cell, boolean left) {
+        int x = cell.getX();
+        int y = cell.getY();
+        Map<IntCouple, ViewCellValues> sentCells = new HashMap<>();
+        boolean defeat = false;
 
+        if (left) {
+            if (statusField[x][y] == StatusCellValue.UNTOUCHED) {
+                if ((field[x][y] >= ONE) && (field[x][y] <= EIGHT)) {
+                    sentCells.put(cell, ViewCellValues.getValueOfNumber(field[x][y]));
+                    statusField[x][y] = StatusCellValue.TOUCHED;
+                } else if (field[x][y] == MINE) {
+                    defeat = true;
+                    touchMineCell(sentCells);
+                } else if (field[x][y] == ZERO) {
+                    touchZeroCell(x, y, sentCells);
+                }
+            } else if (statusField[x][y] == StatusCellValue.TOUCHED) {
+                defeat = touchTouchedCell(x, y, sentCells);
+                if (sentCells.size() == 0) {
+                    return;
+                }
+            } else if (statusField[x][y] == StatusCellValue.FLAG) {
+                return;
+            }
+        } else {
+            if (statusField[x][y] == StatusCellValue.UNTOUCHED) {
+                statusField[x][y] = StatusCellValue.FLAG;
+                sentCells.put(cell, ViewCellValues.FLAG);
+            } else if (statusField[x][y] == StatusCellValue.FLAG) {
+                statusField[x][y] = StatusCellValue.UNTOUCHED;
+                sentCells.put(cell, ViewCellValues.UNTOUCHED);
+            } else if (statusField[x][y] == StatusCellValue.TOUCHED) {
+                defeat = touchTouchedCell(x, y, sentCells);
+            }
+        }
+
+        if (defeat) {
+            fieldView.updateCells(sentCells, GameStatus.DEFEAT);
+        } else if (isVictory()) {
+            fieldView.updateCells(sentCells, GameStatus.VICTORY);
+        } else {
+            fieldView.updateCells(sentCells, GameStatus.CONTINUED);
+        }
+    }
+
+
+    private void touchZeroCell(int x, int y, Map<IntCouple, ViewCellValues> sentCells) {
+        if ((x < 0) || (y < 0) || (x > field.length - 1) || (y > field[0].length - 1)) {
+            return;
+        }
+        sentCells.put(new IntCouple(x, y), ViewCellValues.getValueOfNumber(field[x][y]));
+        statusField[x][y] = StatusCellValue.TOUCHED;
+        if (field[x][y] != ZERO) {
+            return;
+        }
+        for (int i = -1; i <= 1; ++i) {
+            for (int j = -1; j <= 1; ++j) {
+                if ((i == 0) && (j == 0)) {
+                    continue;
+                }
+                if (!sentCells.keySet().contains(new IntCouple(x + i, y + j))) {
+                    touchZeroCell(x + i, y + j, sentCells);
+                }
+            }
+        }
+    }
+
+    private void touchMineCell(Map<IntCouple, ViewCellValues> sentCells) {
+        for (int i = 0; i < statusField.length; ++i) {
+            for (int j = 0; j < statusField[0].length; ++j) {
+                if ((field[i][j] == MINE) && (statusField[i][j] == StatusCellValue.UNTOUCHED)) {
+                    sentCells.put(new IntCouple(i, j), ViewCellValues.MINE);
+                } else if ((field[i][j] != MINE) && (statusField[i][j] == StatusCellValue.FLAG)) {
+                    sentCells.put(new IntCouple(i, j), ViewCellValues.NOMINE);
+                }
+            }
+        }
+    }
+
+    private boolean touchTouchedCell(int x, int y, Map<IntCouple, ViewCellValues> sentCells) {
+        if (field[x][y] == ZERO) {
+            return false;
+        }
+
+        int flagsAround = 0;
+        for (int i = -1; i <= 1; ++i) {
+            for (int j = -1; j <= 1; ++j) {
+                if ((i == 0) && (j == 0)) {
+                    continue;
+                }
+                try {
+                    if (statusField[x + i][y + j] == StatusCellValue.FLAG) {
+                        flagsAround++;
+                    }
+                } catch (IndexOutOfBoundsException ignored) {
+                }
+            }
+        }
+        if (flagsAround != field[x][y]) {
+            return false;
+        }
+
+        boolean defeat = false;
+        for (int i = -1; i <= 1; ++i) {
+            for (int j = -1; j <= 1; ++j) {
+                if ((i == 0) && (j == 0)) {
+                    continue;
+                }
+                try {
+                    if (statusField[x + i][y + j] != StatusCellValue.UNTOUCHED) {
+                        continue;
+                    }
+                    if (field[x + i][y + j] == ZERO) {
+                        touchZeroCell(x + i, y + j, sentCells);
+                    } else if ((field[x + i][y + j] >= ONE) && (field[x + i][y + j] <= EIGHT)) {
+                        sentCells.put(new IntCouple(x + i, y + j),
+                                ViewCellValues.getValueOfNumber(field[x + i][y + j]));
+                    } else if (field[x + i][y + j] == MINE) {
+                        if (!defeat) {
+                            touchMineCell(sentCells);
+                            defeat = true;
+                        }
+                    }
+                } catch (IndexOutOfBoundsException ignored) {
+                }
+            }
+        }
+        return defeat;
+    }
+
+    private boolean isVictory() {
+        for (int i = 0; i < statusField.length; ++i) {
+            for (int j = 0; j < statusField[0].length; ++j) {
+                if (statusField[i][j] == StatusCellValue.UNTOUCHED) {
+                    return false;
+                }
+                if ((statusField[i][j] == StatusCellValue.FLAG) && (field[i][j] != MINE)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private enum StatusCellValue {
+        UNTOUCHED, TOUCHED, FLAG
     }
 }
--- task3/src/test/java/ru/cft/focusstart/ryazantsev/logic/FieldLogicTest.java
+++ task3/src/test/java/ru/cft/focusstart/ryazantsev/logic/FieldLogicTest.java
@@ -2,6 +2,8 @@ package ru.cft.focusstart.ryazantsev.logic;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InOrder;
+import org.mockito.Mockito;
 import ru.cft.focusstart.ryazantsev.view.FieldView;
 
 import java.util.HashMap;
@@ -118,11 +120,11 @@ public class FieldLogicTest {
                 {ONE, MINE, ONE}
         };
         fieldLogic = new FieldLogic(smallField, fieldView);
-        expectedCells.put(new IntCouple(2, 0), ViewCellValues.ZERO);
-        expectedCells.put(new IntCouple(1, 0), ViewCellValues.ONE);
+        expectedCells.put(new IntCouple(0, 2), ViewCellValues.ZERO);
+        expectedCells.put(new IntCouple(0, 1), ViewCellValues.ONE);
         expectedCells.put(new IntCouple(1, 1), ViewCellValues.TWO);
         expectedCells.put(new IntCouple(1, 2), ViewCellValues.ONE);
-        fieldLogic.pressCell(new IntCouple(2, 0), true);
+        fieldLogic.pressCell(new IntCouple(0, 2), true);
         verify(fieldView).updateCells(expectedCells, GameStatus.CONTINUED);
 
         expectedCells.clear();
@@ -153,30 +155,32 @@ public class FieldLogicTest {
 
     @Test
     public void testTurnFlagOnOff() {
+        InOrder inOrder = inOrder(fieldView);
         IntCouple cell = new IntCouple(0, 0);
         expectedCells.put(cell, ViewCellValues.FLAG);
         fieldLogic.pressCell(cell, false);
-        verify(fieldView).updateCells(expectedCells, GameStatus.CONTINUED);
+        inOrder.verify(fieldView).updateCells(expectedCells, GameStatus.CONTINUED);
 
-        expectedCells.clear();
         fieldLogic.pressCell(cell, true);
-        verify(fieldView, times(0)).updateCells(any(), any());
+        inOrder.verify(fieldView, times(0)).updateCells(any(), any());
 
         expectedCells.clear();
         expectedCells.put(new IntCouple(0, 0), ViewCellValues.UNTOUCHED);
         fieldLogic.pressCell(new IntCouple(0, 0), false);
-        verify(fieldView).updateCells(expectedCells, GameStatus.CONTINUED);
+        inOrder.verify(fieldView).updateCells(expectedCells, GameStatus.CONTINUED);
     }
 
     @Test
     public void testDefeatWithFlags() {
+        InOrder inOrder = inOrder(fieldView);
         expectedCells.put(new IntCouple(0, 0), ViewCellValues.FLAG);
         fieldLogic.pressCell(new IntCouple(0, 0), false);
-        verify(fieldView).updateCells(expectedCells, GameStatus.CONTINUED);
+        inOrder.verify(fieldView).updateCells(expectedCells, GameStatus.CONTINUED);
 
+        expectedCells.clear();
         expectedCells.put(new IntCouple(0, 1), ViewCellValues.FLAG);
         fieldLogic.pressCell(new IntCouple(0, 1), false);
-        verify(fieldView).updateCells(expectedCells, GameStatus.CONTINUED);
+        inOrder.verify(fieldView).updateCells(expectedCells, GameStatus.CONTINUED);
 
         expectedCells.clear();
         expectedCells.put(new IntCouple(0, 0), ViewCellValues.NOMINE);
@@ -190,7 +194,7 @@ public class FieldLogicTest {
 
         fieldLogic.pressCell(new IntCouple(2, 4), true);
 
-        verify(fieldView).updateCells(expectedCells, GameStatus.DEFEAT);
+        inOrder.verify(fieldView).updateCells(expectedCells, GameStatus.DEFEAT);
     }
 
     @Test
@@ -250,24 +254,26 @@ public class FieldLogicTest {
 
     @Test
     public void testClickAroundNothing() {
+        InOrder inOrder = inOrder(fieldView);
         expectedCells.put(new IntCouple(0, 2), ViewCellValues.ONE);
         fieldLogic.pressCell(new IntCouple(0, 2), true);
-        verify(fieldView).updateCells(expectedCells, GameStatus.CONTINUED);
+        inOrder.verify(fieldView).updateCells(expectedCells, GameStatus.CONTINUED);
 
         fieldLogic.pressCell(new IntCouple(0, 2), true);
-        verify(fieldView, times(0)).updateCells(any(), any());
+        inOrder.verify(fieldView, times(0)).updateCells(any(), any());
     }
 
     @Test
     public void testClickAroundWrong() {
+        InOrder inOrder = inOrder(fieldView);
         expectedCells.put(new IntCouple(7, 6), ViewCellValues.ONE);
         fieldLogic.pressCell(new IntCouple(7, 6), true);
-        verify(fieldView).updateCells(expectedCells, GameStatus.CONTINUED);
+        inOrder.verify(fieldView).updateCells(expectedCells, GameStatus.CONTINUED);
 
         expectedCells.clear();
         expectedCells.put(new IntCouple(7, 5), ViewCellValues.FLAG);
         fieldLogic.pressCell(new IntCouple(7, 5), false);
-        verify(fieldView).updateCells(expectedCells, GameStatus.CONTINUED);
+        inOrder.verify(fieldView).updateCells(expectedCells, GameStatus.CONTINUED);
 
         expectedCells.clear();
         expectedCells.put(new IntCouple(0, 1), ViewCellValues.MINE);
@@ -282,6 +288,8 @@ public class FieldLogicTest {
         expectedCells.put(new IntCouple(6, 5), ViewCellValues.ONE);
         expectedCells.put(new IntCouple(6, 7), ViewCellValues.ONE);
         expectedCells.put(new IntCouple(7, 7), ViewCellValues.ONE);
-        verify(fieldView).updateCells(expectedCells, GameStatus.DEFEAT);
+        fieldLogic.pressCell(new IntCouple(7, 6), true);
+
+        inOrder.verify(fieldView).updateCells(expectedCells, GameStatus.DEFEAT);
     }
 }
