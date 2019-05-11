2ae85ffa58361b535346296c1d5515be0492ba8f
Yegor Ryazancev <egorryazancev1908@mail.ru>
2019-05-11 15:04:53 +0700

    Создан класс GameConstant

---
+++ task3/src/main/java/ru/cft/focusstart/ryazantsev/GameConstant.java
@@ -0,0 +1,13 @@
+package ru.cft.focusstart.ryazantsev;
+
+import ru.cft.focusstart.ryazantsev.logic.IntCouple;
+
+public class GameConstant {
+    public static final IntCouple EASY_SIZE = new IntCouple(10, 10);
+    public static final IntCouple MIDDLE_SIZE = new IntCouple(15, 15);
+    public static final IntCouple HARD_SIZE = new IntCouple(20, 20);
+
+    public static final int EASY_MINE_COUNT = 20;
+    public static final int MIDDLE_MINE_COUNT = 40;
+    public static final int HARD_MINE_COUNT = 60;
+}
