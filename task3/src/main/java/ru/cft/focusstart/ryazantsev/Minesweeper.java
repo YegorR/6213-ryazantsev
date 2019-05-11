1b9d001c0801ea3e9599bfd8af4251c3d662efcc
Yegor Ryazancev <egorryazancev1908@mail.ru>
2019-05-11 15:05:59 +0700

    Создан класс Minesweeper

---
+++ task3/src/main/java/ru/cft/focusstart/ryazantsev/Minesweeper.java
@@ -0,0 +1,9 @@
+package ru.cft.focusstart.ryazantsev;
+
+import ru.cft.focusstart.ryazantsev.view.WindowCreator;
+
+public class Minesweeper {
+    public static void main(String[] args) {
+        new WindowCreator().CreateWindow();
+    }
+}
