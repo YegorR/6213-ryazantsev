2e56cf943ebec1123feb0a28ea146e1cf8300b41
Yegor Ryazancev <egorryazancev1908@mail.ru>
2019-05-25 12:04:54 +0700

    Перечисление StatusCellValue удалено и заменено ViewCellValue

--- task3/src/main/java/ru/cft/focusstart/ryazantsev/logic/FieldLogic.java
+++ task3/src/main/java/ru/cft/focusstart/ryazantsev/logic/FieldLogic.java
@@ -13,16 +13,16 @@ import java.util.Map;
 
 public class FieldLogic {
     private int[][] field;
-    private StatusCellValue[][] statusField;
+    private ViewCellValue[][] statusField;
     private int minesCount;
 
     public FieldLogic(int[][] field, int minesCount) {
         this.field = field;
         this.minesCount = minesCount;
 
-        statusField = new StatusCellValue[field.length][field[0].length];
-        for (StatusCellValue[] row : statusField) {
-            Arrays.fill(row, StatusCellValue.UNTOUCHED);
+        statusField = new ViewCellValue[field.length][field[0].length];
+        for (ViewCellValue[] row : statusField) {
+            Arrays.fill(row, ViewCellValue.UNTOUCHED);
         }
     }
 
@@ -33,26 +33,26 @@ public class FieldLogic {
         boolean defeat = false;
 
         if (left) {
-            if (statusField[x][y] == StatusCellValue.UNTOUCHED) {
+            if (statusField[x][y] == ViewCellValue.UNTOUCHED) {
                 if ((field[x][y] >= ONE) && (field[x][y] <= EIGHT)) {
                     sentCells.put(cell, ViewCellValue.getValueOfNumber(field[x][y]));
-                    statusField[x][y] = StatusCellValue.TOUCHED;
+                    statusField[x][y] = ViewCellValue.getValueOfNumber(field[x][y]);
                 } else if (field[x][y] == MINE) {
                     defeat = true;
                     touchMineCell(sentCells);
                 } else if (field[x][y] == ZERO) {
                     touchZeroCell(x, y, sentCells);
                 }
-            } else if (statusField[x][y] == StatusCellValue.TOUCHED) {
+            } else if (statusField[x][y] != ViewCellValue.FLAG) {
                 defeat = touchTouchedCell(x, y, sentCells);
             }
         } else {
-            if (statusField[x][y] == StatusCellValue.UNTOUCHED) {
-                statusField[x][y] = StatusCellValue.FLAG;
+            if (statusField[x][y] == ViewCellValue.UNTOUCHED) {
+                statusField[x][y] = ViewCellValue.FLAG;
                 sentCells.put(cell, ViewCellValue.FLAG);
                 minesCount--;
-            } else if (statusField[x][y] == StatusCellValue.FLAG) {
-                statusField[x][y] = StatusCellValue.UNTOUCHED;
+            } else if (statusField[x][y] == ViewCellValue.FLAG) {
+                statusField[x][y] = ViewCellValue.UNTOUCHED;
                 sentCells.put(cell, ViewCellValue.UNTOUCHED);
                 minesCount++;
             }
@@ -73,7 +73,7 @@ public class FieldLogic {
             return;
         }
         sentCells.put(new IntCouple(x, y), ViewCellValue.getValueOfNumber(field[x][y]));
-        statusField[x][y] = StatusCellValue.TOUCHED;
+        statusField[x][y] = ViewCellValue.getValueOfNumber(field[x][y]);
         if (field[x][y] != ZERO) {
             return;
         }
@@ -92,9 +92,9 @@ public class FieldLogic {
     private void touchMineCell(Map<IntCouple, ViewCellValue> sentCells) {
         for (int i = 0; i < statusField.length; ++i) {
             for (int j = 0; j < statusField[0].length; ++j) {
-                if ((field[i][j] == MINE) && (statusField[i][j] == StatusCellValue.UNTOUCHED)) {
+                if ((field[i][j] == MINE) && (statusField[i][j] == ViewCellValue.UNTOUCHED)) {
                     sentCells.put(new IntCouple(i, j), ViewCellValue.MINE);
-                } else if ((field[i][j] != MINE) && (statusField[i][j] == StatusCellValue.FLAG)) {
+                } else if ((field[i][j] != MINE) && (statusField[i][j] == ViewCellValue.FLAG)) {
                     sentCells.put(new IntCouple(i, j), ViewCellValue.NO_MINE);
                 }
             }
@@ -113,7 +113,7 @@ public class FieldLogic {
                     continue;
                 }
                 try {
-                    if (statusField[x + i][y + j] == StatusCellValue.FLAG) {
+                    if (statusField[x + i][y + j] == ViewCellValue.FLAG) {
                         flagsAround++;
                     }
                 } catch (IndexOutOfBoundsException ignored) {
@@ -131,7 +131,7 @@ public class FieldLogic {
                     continue;
                 }
                 try {
-                    if (statusField[x + i][y + j] != StatusCellValue.UNTOUCHED) {
+                    if (statusField[x + i][y + j] != ViewCellValue.UNTOUCHED) {
                         continue;
                     }
                     if (field[x + i][y + j] == ZERO) {
@@ -139,7 +139,7 @@ public class FieldLogic {
                     } else if ((field[x + i][y + j] >= ONE) && (field[x + i][y + j] <= EIGHT)) {
                         sentCells.put(new IntCouple(x + i, y + j),
                                 ViewCellValue.getValueOfNumber(field[x + i][y + j]));
-                        statusField[x + i][y + j] = StatusCellValue.TOUCHED;
+                        statusField[x + i][y + j] = ViewCellValue.getValueOfNumber(field[x + i][y + j]);
                     } else if (field[x + i][y + j] == MINE) {
                         if (!defeat) {
                             touchMineCell(sentCells);
@@ -156,18 +156,14 @@ public class FieldLogic {
     private boolean isVictory() {
         for (int i = 0; i < statusField.length; ++i) {
             for (int j = 0; j < statusField[0].length; ++j) {
-                if (statusField[i][j] == StatusCellValue.UNTOUCHED) {
+                if (statusField[i][j] == ViewCellValue.UNTOUCHED) {
                     return false;
                 }
-                if ((statusField[i][j] == StatusCellValue.FLAG) && (field[i][j] != MINE)) {
+                if ((statusField[i][j] == ViewCellValue.FLAG) && (field[i][j] != MINE)) {
                     return false;
                 }
             }
         }
         return true;
-    }
-
-    private enum StatusCellValue {
-        UNTOUCHED, TOUCHED, FLAG
     }
 }
