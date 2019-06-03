5b6b1be9f6dd804286e0b6d2ebeb6ad6175537c1
Yegor Ryazancev <egorryazancev1908@mail.ru>
2019-06-03 11:01:20 +0700

    Task 4_2 выполнено

--- task4_2/src/main/java/ru/cft/focusstart/ryazantsev/Stock.java
+++ task4_2/src/main/java/ru/cft/focusstart/ryazantsev/Stock.java
@@ -1,4 +1,34 @@
 package ru.cft.focusstart.ryazantsev;
 
+import ru.cft.focusstart.ryazantsev.exception.StockIsEmptyException;
+import ru.cft.focusstart.ryazantsev.exception.StockIsFullException;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Stock {
+    private static final int size = 8;
+    private List<Resource> resources = new ArrayList<>();
+
+    boolean isEmpty() {
+        return resources.isEmpty();
+    }
+
+    boolean isFull() {
+        return resources.size() == size;
+    }
+
+    void addResource(Resource resource) throws StockIsFullException {
+        if (resources.size() == size) {
+            throw new StockIsFullException();
+        }
+        resources.add(resource);
+    }
+
+    Resource takeResource() throws StockIsEmptyException {
+        if (resources.isEmpty()) {
+            throw new StockIsEmptyException();
+        }
+        return resources.remove(resources.size() - 1);
+    }
 }
