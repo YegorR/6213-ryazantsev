71e5e37fc03b69c4f6701a9bb8d35ad992063bd8
Yegor Ryazancev <egorryazancev1908@mail.ru>
2019-06-20 15:13:41 +0700

    GUI скомпоновано

--- task5/Client/src/main/java/ru/cft/focusstart/ryazantsev/client/MainWindow.java
+++ task5/Client/src/main/java/ru/cft/focusstart/ryazantsev/client/MainWindow.java
@@ -1,5 +1,8 @@
 package ru.cft.focusstart.ryazantsev.client;
 
+import ru.cft.focusstart.ryazantsev.client.widget.ChatMenuBar;
+import ru.cft.focusstart.ryazantsev.client.widget.InputWidget;
+import ru.cft.focusstart.ryazantsev.client.widget.MembersListWidget;
 import ru.cft.focusstart.ryazantsev.client.widget.OutputWidget;
 
 import javax.swing.*;
@@ -7,21 +10,49 @@ import java.awt.*;
 
 public class MainWindow {
     private JFrame frame;
+    private OutputWidget outputWidget;
+    private InputWidget inputWidget;
+    private MembersListWidget membersListWidget;
+
     public MainWindow() {
         frame = new JFrame();
         frame.setTitle("Chat");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setResizable(false);
-
-
-        OutputWidget outputWidget = new OutputWidget();
-        frame.setLayout(new BorderLayout());
-        frame.add(outputWidget.getWidget());
+        createGUI();
 
         frame.pack();
         frame.setVisible(true);
-        for(int i = 0; i < 10; ++i) {
-            outputWidget.writeMessage(null);
-        }
+    }
+
+    private void createGUI() {
+        createMenu();
+        frame.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+
+        outputWidget = new OutputWidget();
+        c.gridx = 0; c.gridy = 0;
+        frame.add(outputWidget.getWidget(), c);
+
+        membersListWidget = new MembersListWidget();
+        c.gridx = 1; c.gridy = 0;
+        frame.add(membersListWidget.getWidget(), c);
+
+        inputWidget = new InputWidget();
+        c.gridx = 0; c.gridy = 1;
+        frame.add(inputWidget.getWidget(), c);
+
+        JButton button = new JButton("Отправить");
+        c.gridx = 1; c.gridy = 1;
+        frame.add(button, c);
+    }
+
+    private void createMenu() {
+        ChatMenuBar myMenu = new ChatMenuBar();
+        frame.setJMenuBar(myMenu.getMenuBar());
+        myMenu.addExitListener(e -> System.out.println("Выход"));
+        myMenu.addInChatListener(e -> System.out.println("Зашли в чат"));
+        myMenu.addOutChatListener(e -> System.out.println("Вышли из чата"));
+        myMenu.enableOutChat(false);
     }
 }
--- task5/Client/src/main/java/ru/cft/focusstart/ryazantsev/client/widget/OutputWidget.java
+++ task5/Client/src/main/java/ru/cft/focusstart/ryazantsev/client/widget/OutputWidget.java
@@ -3,9 +3,7 @@ package ru.cft.focusstart.ryazantsev.client.widget;
 import ru.cft.focusstart.ryazantsev.common.Message;
 
 import javax.swing.*;
-import javax.swing.text.html.HTMLDocument;
 import java.awt.*;
-import java.text.SimpleDateFormat;
 
 
 public class OutputWidget {
@@ -21,7 +19,7 @@ public class OutputWidget {
         scrollPane = new JScrollPane(editorPane);
         scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
         scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
-        scrollPane.setPreferredSize(new Dimension(300, 400));
+        scrollPane.setPreferredSize(new Dimension(300, 300));
         scrollPane.setVerticalScrollBar(new JScrollBar());
     }
 
