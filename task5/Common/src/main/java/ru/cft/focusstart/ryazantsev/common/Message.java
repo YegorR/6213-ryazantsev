34dad8533fffcb8bd83024d4b3e149e5b67945eb
Yegor Ryazancev <egorryazancev1908@mail.ru>
2019-06-26 01:48:19 +0700

    Добавлено пара комментариев

--- task5/Common/src/main/java/ru/cft/focusstart/ryazantsev/common/Message.java
+++ task5/Common/src/main/java/ru/cft/focusstart/ryazantsev/common/Message.java
@@ -3,9 +3,11 @@ package ru.cft.focusstart.ryazantsev.common;
 import java.util.Date;
 
 public class Message {
+    //ERROR служит зарезервированным значением для ошибок, в данный момент фактически игнорируется
     public enum MessageType {
         SUCCESSFUL_CONNECT, BAD_NAME, SERVER_OUT, MESSAGE, NEW_MEMBER, OLD_MEMBER, GONE_MEMBER, ERROR
     }
+    //Часть setter'ов и пустой конструктор нужны для Jackson
 
     private String text;
     private String name;
