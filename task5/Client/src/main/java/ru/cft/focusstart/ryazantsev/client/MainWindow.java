08e1d4e7d90e19d0c27e45fb8a00e3e57bf4789f
Yegor Ryazancev <egorryazancev1908@mail.ru>
2019-06-30 01:15:34 +0700

    Большинством событием теперь управляют состояния. При отключении со стороны сервера клиент пытается коннектиться обратно

--- task5/Client/src/main/java/ru/cft/focusstart/ryazantsev/client/MainWindow.java
+++ task5/Client/src/main/java/ru/cft/focusstart/ryazantsev/client/MainWindow.java
@@ -8,6 +8,8 @@ import static ru.cft.focusstart.ryazantsev.common.Message.MessageType.*;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.IOException;
 
 class MainWindow {
@@ -22,54 +24,24 @@ class MainWindow {
     private LoginDialog loginDialog;
     private LoginDialog.LoginData loginData;
 
+    private State state;
+
+    private void changeState(State state) {
+        this.state = state;
+        state.run();
+    }
+
     MainWindow() {
+        createGUI();
+        createNetManager();
+        changeState(new LoginDialogState());
+    }
+
+    private void createGUI() {
         frame = new JFrame();
         frame.setTitle("Chat");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setResizable(false);
-        createGUI();
-        netManager = new NetManager(this::receiveConnection, this::receiveMessageStatus);
-        frame.pack();
-        frame.setVisible(true);
-        new SwingWorker<Void, Void>() {
-            @Override
-            protected Void doInBackground() {
-                boolean isInterrupted = false;
-                while (!isInterrupted) {
-                    try {
-                        if (netManager.isReady()) {
-                            receiveMessage(netManager.getMessage());
-                        }
-                        Thread.sleep(100);
-                    } catch (InterruptedException ex) {
-                        isInterrupted = true;
-                    } catch (IOException ex) {
-                        closeConnectionWithError();
-                    }
-                }
-                return null;
-            }
-        }.execute();
-
-        createLoginDialog();
-    }
-
-    private void createLoginDialog() {
-        new SwingWorker<Void, Void>() {
-            @Override
-            protected Void doInBackground() {
-                if (loginData == null) {
-                    loginDialog = new LoginDialog(frame, MainWindow.this::login);
-                } else {
-                    loginDialog = new LoginDialog(frame, MainWindow.this::login, loginData);
-                }
-                loginDialog.setVisible();
-                return null;
-            }
-        }.execute();
-    }
-
-    private void createGUI() {
         createMenu();
         frame.setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
@@ -96,102 +68,59 @@ class MainWindow {
             message.setName(loginData.getName());
             netManager.sendMessage(message);
         });
-        button.setEnabled(false);
         c.gridx = 1;
         c.gridy = 1;
         frame.add(button, c);
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private void createNetManager() {
+        netManager = new NetManager(this::receiveConnection, this::receiveMessageStatus);
+        new SwingWorker<Void, Void>() {
+            @Override
+            protected Void doInBackground() {
+                boolean isInterrupted = false;
+                while (!isInterrupted) {
+                    try {
+                        if (netManager.isReady()) {
+                            receiveMessage(netManager.getMessage());
+                        }
+                        Thread.sleep(100);
+                    } catch (InterruptedException ex) {
+                        isInterrupted = true;
+                    } catch (IOException ex) {
+                        closeConnectionWithError();
+                    }
+                }
+                return null;
+            }
+        }.execute();
     }
 
     private void createMenu() {
         menu = new ChatMenuBar();
         frame.setJMenuBar(menu.getMenuBar());
         menu.addExitListener(e -> System.exit(0));
-        menu.addInChatListener(e -> createLoginDialog());
-        menu.addOutChatListener(e -> {
-            netManager.close();
-            button.setEnabled(false);
-            menu.switchMode(true);
-        });
-        menu.switchMode(true);
+        menu.addInChatListener(e -> clickInChatMenu());
+        menu.addOutChatListener(e -> clickOutChatMenu());
     }
 
-    private void login(LoginDialog.LoginData loginData) {
-        this.loginData = loginData;
-        String host = loginData.getHost();
-        int port = loginData.getPort();
-        String name = loginData.getName();
+    private void clickInChatMenu() {
+        changeState(new LoginDialogState());
+    }
 
-        if (host.isEmpty()) {
-            loginDialog.writeErrorMessage("Введите адрес хоста");
-            return;
-        }
-        if (name.isEmpty()) {
-            loginDialog.writeErrorMessage("Введите ник");
-            return;
-        }
-        if (port == 0) {
-            loginDialog.writeErrorMessage("Введите корректный порт");
-            return;
-        }
-        loginDialog.setEnabledButton(false);
-        netManager.connect(host, port);
+    private void clickOutChatMenu() {
+        changeState(new DoingNothingState());
     }
 
     private void receiveConnection(boolean isConnected) {
-        if (!isConnected) {
-            loginDialog.writeErrorMessage("Не удалось подключиться к серверу");
-            loginDialog.setEnabledButton(true);
-        } else {
-            Message message = new Message(Message.MessageType.NEW_MEMBER);
-            message.setName(loginData.getName());
-            netManager.sendMessage(message);
-        }
+        state.receiveConnection(isConnected);
     }
 
     private void receiveMessage(Message message) {
-        Message.MessageType messageType = message.getMessageType();
-        switch (messageType) {
-            case SUCCESSFUL_CONNECT:
-                if (loginDialog.isVisible()) {
-                    loginDialog.destroy();
-                    menu.switchMode(false);
-                    button.setEnabled(true);
-                    membersListWidget.clear();
-                    outputWidget.clear();
-                } else {
-                    netManager.sendMessage(new Message(ERROR));
-                }
-                break;
-            case MESSAGE:
-                outputWidget.printMessage(message);
-                break;
-            case NEW_MEMBER:
-                outputWidget.printMessage(message);
-                membersListWidget.addMember(message.getName());
-                break;
-            case GONE_MEMBER:
-                outputWidget.printMessage(message);
-                membersListWidget.removeMember(message.getName());
-                break;
-            case BAD_NAME:
-                if (!loginDialog.isVisible()) {
-                    netManager.sendMessage(new Message(ERROR));
-                } else {
-                    loginDialog.writeErrorMessage("Выберите другое имя");
-                    loginDialog.setEnabledButton(true);
-                }
-                break;
-            case OLD_MEMBER:
-                membersListWidget.addMember(message.getName());
-                break;
-            case SERVER_OUT:
-                menu.switchMode(true);
-                outputWidget.printMessage(message);
-                button.setEnabled(false);
-                netManager.close();
-            case ERROR:
-                break;
-        }
+        state.receiveMessage(message);
     }
 
     private void receiveMessageStatus(boolean successful) {
@@ -201,13 +130,230 @@ class MainWindow {
     }
 
     private void closeConnectionWithError() {
-        netManager.close();
-        if (loginDialog.isVisible()) {
+        state.handleConnectionError();
+    }
+
+
+    private interface State {
+        void run();
+
+        void handleConnectionError();
+
+        void receiveMessage(Message message);
+
+        void receiveConnection(boolean isConnected);
+    }
+
+
+    private class LoginDialogState implements State {
+        private boolean closedByUser = false;
+
+        @Override
+        public void run() {
+            createLoginDialog();
+            button.setEnabled(false);
+            menu.switchMode(true);
+        }
+
+        @Override
+        public void handleConnectionError() {
+            netManager.close();
             loginDialog.writeErrorMessage("Произошла ошибка соединения");
-        } else {
+        }
+
+        @Override
+        public void receiveMessage(Message message) {
+            Message.MessageType messageType = message.getMessageType();
+            switch (messageType) {
+                case SUCCESSFUL_CONNECT:
+                    closedByUser = false;
+                    loginDialog.destroy();
+                    changeState(new OnlineState());
+                    break;
+                case BAD_NAME:
+                    loginDialog.writeErrorMessage("Выберите другое имя");
+                    loginDialog.setEnabledButton(true);
+                    break;
+            }
+        }
+
+        @Override
+        public void receiveConnection(boolean isConnected) {
+            if (!isConnected) {
+                loginDialog.writeErrorMessage("Не удалось подключиться к серверу");
+                loginDialog.setEnabledButton(true);
+            } else {
+                Message message = new Message(Message.MessageType.NEW_MEMBER);
+                message.setName(loginData.getName());
+                netManager.sendMessage(message);
+            }
+        }
+
+        private boolean isClosedByUser() {
+            return closedByUser;
+        }
+
+        private void createLoginDialog() {
+            new SwingWorker<Void, Void>() {
+                @Override
+                protected Void doInBackground() {
+                    if (loginData == null) {
+                        loginDialog = new LoginDialog(frame, LoginDialogState.this::login);
+                    } else {
+                        loginDialog = new LoginDialog(frame, LoginDialogState.this::login, loginData);
+                    }
+                    loginDialog.addCloseListener(new WindowAdapter() {
+                        @Override
+                        public void windowClosed(WindowEvent e) {
+                            super.windowClosed(e);
+                            if (isClosedByUser()) {
+                                changeState(new DoingNothingState());
+                            }
+                        }
+                    });
+                    loginDialog.setVisible();
+                    return null;
+                }
+            }.execute();
+        }
+
+        private void login(LoginDialog.LoginData loginData) {
+            MainWindow.this.loginData = loginData;
+            String host = loginData.getHost();
+            int port = loginData.getPort();
+            String name = loginData.getName();
+
+            if (host.isEmpty()) {
+                loginDialog.writeErrorMessage("Введите адрес хоста");
+                return;
+            }
+            if (name.isEmpty()) {
+                loginDialog.writeErrorMessage("Введите ник");
+                return;
+            }
+            if (port == 0) {
+                loginDialog.writeErrorMessage("Введите корректный порт");
+                return;
+            }
+            loginDialog.setEnabledButton(false);
+            netManager.connect(host, port);
+        }
+    }
+
+
+    private class DoingNothingState implements State {
+        @Override
+        public void run() {
+            netManager.close();
+            menu.switchMode(true);
+            button.setEnabled(false);
+        }
+
+        @Override
+        public void handleConnectionError() {
             JOptionPane.showMessageDialog(frame, "Произошла ошибка соединения", "Ошибка",
                     JOptionPane.WARNING_MESSAGE);
         }
 
+        @Override
+        public void receiveConnection(boolean isConnected) {
+        }
+
+        @Override
+        public void receiveMessage(Message message) {
+        }
+    }
+
+
+    private class TryingConnectState implements State {
+        private String host = loginData.getHost();
+        private int port = loginData.getPort();
+
+        @Override
+        public void run() {
+            button.setEnabled(false);
+            menu.switchMode(true);
+            netManager.close();
+            tryConnect();
+        }
+
+        @Override
+        public void handleConnectionError() {
+        }
+
+        @Override
+        public void receiveConnection(boolean isConnected) {
+            if (isConnected) {
+                Message message = new Message(Message.MessageType.NEW_MEMBER);
+                message.setName(loginData.getName());
+                netManager.sendMessage(message);
+            } else {
+                tryConnect();
+            }
+        }
+
+        @Override
+        public void receiveMessage(Message message) {
+            Message.MessageType messageType = message.getMessageType();
+            switch (messageType) {
+                case SUCCESSFUL_CONNECT:
+                    changeState(new OnlineState());
+                    break;
+                case BAD_NAME:
+                    changeState(new LoginDialogState());
+                    state.receiveMessage(message);
+                    break;
+            }
+        }
+
+        private void tryConnect() {
+            netManager.connect(host, port);
+        }
+    }
+
+
+    private class OnlineState implements State {
+        @Override
+        public void run() {
+            menu.switchMode(false);
+            button.setEnabled(true);
+            membersListWidget.clear();
+            outputWidget.clear();
+        }
+
+        @Override
+        public void handleConnectionError() {
+            JOptionPane.showMessageDialog(frame, "Произошла ошибка соединения", "Ошибка",
+                    JOptionPane.WARNING_MESSAGE);
+            changeState(new DoingNothingState());
+        }
+
+        @Override
+        public void receiveConnection(boolean isConnected) {
+        }
+
+        @Override
+        public void receiveMessage(Message message) {
+            Message.MessageType messageType = message.getMessageType();
+            switch (messageType) {
+                case MESSAGE:
+                    outputWidget.printMessage(message);
+                    break;
+                case NEW_MEMBER:
+                    outputWidget.printMessage(message);
+                    membersListWidget.addMember(message.getName());
+                    break;
+                case GONE_MEMBER:
+                    outputWidget.printMessage(message);
+                    membersListWidget.removeMember(message.getName());
+                    break;
+                case OLD_MEMBER:
+                    membersListWidget.addMember(message.getName());
+                    break;
+                case SERVER_OUT:
+                    outputWidget.printMessage(message);
+                    changeState(new TryingConnectState());
+            }
+        }
     }
 }
